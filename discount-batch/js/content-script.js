cf926360297d94ddce0a873456b0680947a171fa
libaojie <libaojie@hirisun.com>
2021-04-23 16:48:14 +0800

    初始化

---
+++ discount-batch/js/content-script.js
@@ -0,0 +1,6 @@
+﻿(function() {
+	// 进入目标网址初始化
+	console.log('这是 simple-chrome-plugin-demo 的content-script！');
+	// alert("呵呵呵")
+	// alert($("#su").size());
+})();
---
+++ discount-batch/js/popup.js
@@ -0,0 +1,8 @@
+﻿
+// 右上角脚本
+(function() {
+    console.log('你好，我是popup！');
+    alert($("#su").size());
+    alert("end");
+});
+
